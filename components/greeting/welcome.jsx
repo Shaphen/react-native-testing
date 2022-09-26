edf18df7e7dfc7ae27447837ab618d0440364c8e
Shaphen <swpburn@gmail.com>
2022-09-26 14:37:28 -0700

    rename file paths and abstract styling

--- components/greeting/welcome.jsx
+++ components/greeting/welcome.jsx
@@ -2,16 +2,14 @@ import {
     Text,
     View,
 } from 'react-native';
+import styles from "./greeting_style.js";
 
 const Welcome = () => {
     return (
         <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
-          <Text>This is the Welcome Component!</Text>
+          style={ styles.container }
+        >
+            <Text>This is the Welcome Component!</Text>
         </View>
     );
 };
