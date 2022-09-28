8327ca2e7956bff82bd33264e11dfbf348477b0b
Shaphen <swpburn@gmail.com>
2022-09-28 15:19:20 -0700

    Add customized icons to navigation container

--- components/App.js
+++ components/App.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import ListView from "./main/list_view";
 import AccountView from "./main/account_view";
 import SavedView from "./main/saved_items_view"
@@ -16,20 +16,23 @@ import { View } from 'react-native';
 import IconE from 'react-native-vector-icons/Entypo';
 import IconA from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const homeIcon = <IconE name="home" size={ 30 } color="#0009" />;
-const bookmarkIcon = <IconE name="bookmark" size={ 30 } color="#0009" />;
-const accountIcon = <IconA name="account" size={ 35 } color="#0009" />;
+const homeIcon = <IconE name="home" size={ 26 } color="#0009" />;
+const bookmarkIcon = <IconE name="bookmark" size={ 28 } color="#0009" />;
+const accountIcon = <IconA name="account" size={ 28 } color="#0009" />;
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="List" component={ ListView } tabBarIcon={ homeIcon } />
-          <Tab.Screen name="Saved" component={ SavedView } />
-          <Tab.Screen name="Account" component={ AccountView } />
+          <Tab.Screen name="List" component={ ListView } options = {() => 
+            ({tabBarIcon: () => homeIcon})} />
+          <Tab.Screen name="Saved" component={ SavedView } options = {() => 
+            ({tabBarIcon: () => bookmarkIcon})} />
+          <Tab.Screen name="Account" component={ AccountView } options = {() => 
+            ({tabBarIcon: () => accountIcon})} />
         </Tab.Navigator>
       </NavigationContainer>
     </View>
