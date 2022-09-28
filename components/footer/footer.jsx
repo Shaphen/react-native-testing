fc7f356d4aaf9101436de8392082368bfd64996c
Shaphen <swpburn@gmail.com>
2022-09-28 10:19:00 -0700

    Add working icon connection to react native app - note: Need to be running on Java 11 to build successfully

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text } from "react-native";
 import styles from "./footer_style";
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
 
 const Footer = () => {
 
@@ -9,6 +11,8 @@ const Footer = () => {
             <Text style={ styles.footerText }>
                 This is the Footer!
             </Text>
+
+            <Text>Icon: { myIcon }</Text>
         </View>
     )
 }
